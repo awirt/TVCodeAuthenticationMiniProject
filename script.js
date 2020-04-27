0d5aa3c1a981350191c64b64834cfc09ae945198
Andrew Wirt <15827151+awirt@users.noreply.github.com>
2020-04-26 23:50:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
-console.log("Script is linked!");
-
 //Containers and Buttons:
 var container1 = $(".firstNum");
 var container2 = $(".secNum");
 var container3 = $(".thirdNum");
 var container4 = $(".fourthNum");
-var submitBtn = $("submitBtn");
-var textField = $("textField");
-
-//Function Declarations:
-function randomNum(){
-  var randDecimal = Math.random();
-  randDecimal *= 9;
-  return(Math.floor(randDecimal));
-}
-
-function checkCode(event){
-  event.preventDefault();
-  console.log("The button worked.");
-//   var userInput = textField.val();
-//   console.log(userInput);
-}
+var submitBtn = $(".submitBtn");
+var textField = $(".textField");
 
 //Function Calls:
 container1.append(randomNum());
@@ -30,6 +14,42 @@ container4.append(randomNum());
 
 //Event Listeners:
 submitBtn.on("click", checkCode);
+
+//Function Declarations & Event Handlers:
+function randomNum(){
+  var randDecimal = Math.random();
+  randDecimal *= 9; return(Math.floor(randDecimal));
+}
+
+function checkCode(event){
+  event.preventDefault();
+  $(".outcome")[0].innerHTML = "";
+  var userInput = textField.val();
+  var compInput = stringNums();
+  action(userInput,compInput);
+  //compares user input against the nums
+  //displays a confirmation message in the container
+}
+
+function stringNums(){
+  var num1 = $(".firstNum")[0].innerHTML;
+  var num2 = $(".secNum")[0].innerHTML;
+  var num3 = $(".thirdNum")[0].innerHTML;
+  var num4 = $(".fourthNum")[0].innerHTML;
+  return(`${num1}${num2}${num3}${num4}`);
+}
+
+function action(num1, num2){
+  var container = $(".outcome");
+  if (num1 === num2){
+    container.append("<p>SUCCESS!</p>")
+  } else {
+    container.append("Sorry! There was an error. Please refresh and try again.");
+  }
+}
+
+// console.log($(".firstNum")[0].innerHTML);
+
 
 
 
