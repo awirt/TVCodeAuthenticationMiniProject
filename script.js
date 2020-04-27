c0302d20a58c995d070f0fc8b8684b2b543ac6c9
Andrew Wirt <15827151+awirt@users.noreply.github.com>
2020-04-26 22:43:12 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+//Containers and Buttons:
+var container1 = $(".firstNum");
+var container2 = $(".secNum");
+var container3 = $(".thirdNum");
+var container4 = $(".fourthNum");
+var submitBtn = $("submitBtn");
+var textField = $("textField");
+
+//Function Declarations:
+function randomNum(){
+  var randDecimal = Math.random();
+  randDecimal *= 9;
+  return(Math.floor(randDecimal));
+}
+
+function checkCode(){
+  event.preventDefault();
+  var userInput = textField.val();
+  console.log(userInput);
+}
+
+//Function Calls:
+container1.append(randomNum());
+container2.append(randomNum());
+container3.append(randomNum());
+container4.append(randomNum());
+
+//Event Listeners:
+submitBtn.on("click", checkCode);
+
+
+
